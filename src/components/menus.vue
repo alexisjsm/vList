ef6300164f10852bb41e98182fbbff6b53ef96aa
alexis <cv.alexisjesus@gmail.com>
2019-08-23 23:59:47 +0200

    primer commit

---
+++ src/components/menus.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  header#menus
+    nav.navbar(role='navegation')
+      .navbar-brand
+        a(href='/' class="navbar-item title has-text-grey-dark") vList
+        a(role="button" aria-label="menu" class="navbar-burger burger" @click="isActive = !isActive" :class="{'is-active':isActive}")
+          span(aria-hidden="true" v-for="n in 3")
+
+      div(class="navbar-menu" :class="{'is-active':isActive}")
+        .navbar-end
+          .navbar-item
+            .buttons
+              a.button.is-primary.is-outlined Nueva nota
+</template>
+<script>
+export default {
+  name: 'menus',
+  data () {
+    return {
+      isActive: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.is-outlined{
+  color: purple;
+}
+</style>
---
+++ src/components/notas.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    section#notas.section
+        nota
+</template>
+
+<script>
+import nota from './nota.vue'
+export default {
+  name: 'notas',
+  components: {
+    nota
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
